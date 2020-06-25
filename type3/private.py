ea3f2e06ce143e6418eb604eb5ac484deab1a8ad
Rich Hart <richard.hart@capco.com>
2020-06-24 21:47:07 -0400

    restart

--- type3/private.py
+++
@@ -1,4 +0,0 @@
-# SECURITY WARNING: keep the secret key used in production secret!
-# FIXME: REGENERATE KEY IN PRODUCTION
-SECRET_KEY = 'di-_vuyc60qgcy_yn7e&gm)0*4ph3vz0gffjma&v#$af6dm(@9'
-
--- type3/settings.py
+++
@@ -1,3 +0,0 @@
-INSTALLED_APPS = ['base']
-
-from .local import *
