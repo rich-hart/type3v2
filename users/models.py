e3695537ba70e765f3a005441ee7b9c615040696
Rich Hart <richard.hart@capco.com>
2020-06-25 00:13:38 -0400

    add users

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+from base.models import Base
+
+class Profile(Base):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+    )
+
+#FIXME:  Auto run register_tags for global modules on startup
+
+Profile.register_tags()
