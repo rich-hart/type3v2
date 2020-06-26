3c925946abfef9211821da8c7edf273a7f083ca8
Rich Hart <richard.hart@capco.com>
2020-06-26 14:55:19 -0400

    add action

--- bases/views.py
+++ bases/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import permissions
+
+class IsOwner(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 from rest_framework import viewsets
-
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from bases.views import IsOwner
 from .models import *
 from .serializers import *
 
@@ -11,3 +13,13 @@ class JobViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user.profile,status=Job.Status.CREATED.value)
+
+    @action(
+        detail=True,
+        #methods=['get'],
+        permission_classes=[IsOwner],
+    )
+    def start(self, request, pk):
+        data = {}
+        return Response(data)    
+
