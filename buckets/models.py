68410b3605af3bcd28196478222a25804c981878
Rich Hart <richard.hart@capco.com>
2020-06-26 03:21:31 -0400

    update

--- buckets/models.py
+++ buckets/models.py
@@ -1,4 +1,35 @@
 from bases.models import Base
 
-class Bucket(Base):
+class FSObject(Base):
+    # path
+    # name
     pass
+
+class Bucket(FSObject):
+    pass
+
+class Folder(FSObject):
+    pass
+
+class File(FSObject):
+    format = None
+    pass
+
+class Page(Base):
+    # image = 1-1 Image
+
+    pass
+
+class Image(File):
+    # format = choices: PNG
+    pass
+
+class Audio(File):
+    # format = choices: MP3
+    pass
+
+
+class Video(File):
+    # format = choices: MP4
+    pass
+
