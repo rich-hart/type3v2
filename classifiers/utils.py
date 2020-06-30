0afce111cebc2f0f8e062da263c17c84d2a7214e
Rich Hart <richard.hart@capco.com>
2020-06-30 14:42:23 -0400

    wip

---
+++ classifiers/utils.py
@@ -0,0 +1,24 @@
+import random
+import numpy as np
+from sklearn.model_selection import train_test_split
+from collections import OrderedDict
+from sklearn import svm
+
+
+
+def train(classifier):
+    import ipdb; ipdb.set_trace()
+    train_samples, test_samples, train_labels, test_labels = train_test_split(
+        samples,
+        labels,
+        test_size=classifier.seed,
+        random_state=classifier
+    )
+    classifier._clf = svm.SVC()
+    classifier._clf.fit(train_samples, train_labels)
+  
+    predicted_labels = classifier._clf.predict(test_samples)
+    classifier._accuracy = sum([ expected == returned for expected, returned in zip(test_labels, predicted_labels)])
+    classifier.save() 
+    return classifier.tag
+
