ddf975f1db5960e16b54baceb83df0f476b2364e
Rich Hart <richard.hart@capco.com>
2020-07-01 12:43:53 -0400

    import settings from encrypted yml file

--- project/settings/local.py
+++ project/settings/local.py
@@ -1,6 +1,18 @@
+import os
+import yaml
 
 
-from .private import *
+
+current_file_dir = os.path.dirname(os.path.abspath(__file__))
+private_yml = os.path.join(current_file_dir, 'private.yml')
+
+if os.path.exists(private_yml):
+    with open(private_yml) as fp:
+        private_settings = yaml.load(fp,Loader=yaml.BaseLoader)
+    for key, value in private_settings.items():
+        exec(f"{key} = '{value}'")
+else:
+    from .private import *
 #https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
 
 AWS_STORAGE_BUCKET_NAME = 'test-vvhgiscyyf'
